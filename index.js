615f5f64d1c6598d815942019f1d61655eb614e9
Ryan Travis <rpenchenski@gmail.com>
2018-06-26 18:57:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
+  var response
   if (string.toLowerCase()) {
     response = "I can't hear you!"
   }
