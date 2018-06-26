047b5cc56d73f8b03d47211efea49b69c8126b07
Ryan Travis <rpenchenski@gmail.com>
2018-06-26 19:09:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string) {
  if (string === "I love you, Grandma.") {
    return "I love you, too."
    } else if (string.toLowerCase()) {
-     return "I can't hear you!"
+     return "I can't hear you"
    } else if (string.toUpperCase()) {
      return "YES INDEED!"
    }
