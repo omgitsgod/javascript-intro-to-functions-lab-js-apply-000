f2dfc73a757bc18b1660112ae0c338aeca4a33df
Ryan Travis <rpenchenski@gmail.com>
2018-06-26 19:12:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
  if (string === "I love you, Grandma.") {
    return "I love you, too."
-   } else if (string.toLowerCase()) {
-     return "I can't hear you"
-   } else if (string.toUpperCase()) {
+   } else if (string === string.toLowerCase()) {
+     return "I can't hear you!"
+   } else if (string === string.toUpperCase()) {
      return "YES INDEED!"
    } else {
      return "UH OH"
